62880d3d69133fc7f32cb530ab25141dc3488cce
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-10 23:50:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
-
 function fetchBooks() {
 
-  fetch('https://anapioficeandfire.com/api/books')
-  .then(function(response) {
-    console.log(response);
-    return response.json();
-  })
-  .then(function(json) {
-    console.log(json);
-  });// To pass the tests, don't forget to return your fetch!
+   let books = fetch('https://anapioficeandfire.com/api/books')
+   .then(resp=> resp.json())
+   .then(json => renderBooks(json))
+   return books
+ }
 
-}
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-
+ function renderBooks(books) {
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
+   })
+ }
+
+
+ document.addEventListener('DOMContentLoaded', function() {
+   fetchBooks()
+ })
